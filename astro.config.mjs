df600bebbec095c8e36ae403e5445b74c420f5f2
Exxa1 <60075441+Exxa1@users.noreply.github.com>
2025-11-04 21:32:59 +0100

    changed to tailwind 4, post.css removed, tailwind prettier removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),
-                 sitemap({
+  integrations: [ sitemap({
                   filter: (page) => page !== new URL('/secret/', SITE_URL).href
                  })],
+
   site: SITE_URL,
+
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
       wrap: false,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
